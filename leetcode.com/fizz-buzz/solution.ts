40e03b59979af406cc229596c180ef415c8acbb4
Marijo Macinkovic <3474632+mmacinko@users.noreply.github.com>
2023-06-28 17:00:48 +0200

    leetcode.com fizz-buzz

---
+++ leetcode.com/fizz-buzz/solution.ts
@@ -0,0 +1,14 @@
+function fizzBuzz(n: number): string[] {
+    let answer: string[] = [];
+    for(let i: number = 1; i < n + 1; i++) {
+        if ((i%3 === 0) && (i%5 === 0)) answer.push('FizzBuzz');
+        else if (i%3 === 0) answer.push('Fizz');
+        else if (i%5 === 0) answer.push('Buzz');
+        else answer.push(`${i}`);
+    }
+    return answer;
+};
+
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(15));
